73545ac581a1b8635da24943f97d6535c7e0157c
Ryszard Szopa <ryszard.szopa@gmail.com>
2009-11-29 05:40:40 +0100

    Something that works.

--- blip/blip.py
+++ blip/blip.py
@@ -1,6 +1,16 @@
 from oauth import oauth
 import urllib2
+import urllib
+import httplib
+import json
+import logging
 
+log = logging.getLogger("blip")
+
+from multipart import encode_multipart_formdata
+
+class BlipError(Exception):
+    pass
 
 class Blip(object):
     protocol = "http://"
@@ -14,15 +24,28 @@ class Blip(object):
     def __init__(self, key=None, secret=None, callback=None):
         self.consumer = oauth.OAuthConsumer(key, secret)
         self.callback = callback
+        self.http = httplib.HTTPConnection(self.host, self.port)
 
-    def _request(self, url, headers={}, data=None, timeout=None):
+    def _request(self, url, method='GET', headers={}, data=None, timeout=None):
+        """Make a http request to `url`, with `headers`. If data is
+        not None, make it a POST request. Returns a file-like object.
+
+        """
         headers = dict(headers)
-        request = urllib2.Request(url, data, headers)
-        return urllib2.urlopen(request)
+        headers['User-Agent'] = 'blipetycje'
+        if data:
+            data, content_type = encode_multipart_formdata(data)
+            headers["Content-type"] = content_type
+
+        log.debug(repr((method, url, data, headers),))
+
+        self.http.request(method, url, data, headers)
+        return self.http.getresponse()
 
     def get_request_token(self):
         oa_req = oauth.OAuthRequest.from_consumer_and_token(
             self.consumer,
+            parameters={'oauth_callback': self.callback},
             http_url=self.request_token_url)
         oa_req.sign_request(self.signature_method,
                                   self.consumer,
@@ -41,13 +64,13 @@ class Blip(object):
             http_url=self.authorization_url,
         )
         oauth_request.sign_request(self.signature_method, self.consumer, token)
-        return oauth_request.to_url(), token        
+        return oauth_request.to_url(), token
 
-
-    def authorize(self, token):
+    def authorize(self, token, verifier):
         oa_req = oauth.OAuthRequest.from_consumer_and_token(
             self.consumer,
             token=token,
+            parameters=dict(oauth_verifier=verifier),
             http_url=self.access_token_url
         )
 
@@ -58,5 +81,41 @@ class Blip(object):
         )
         req = self._request(oa_req.to_url())
         res = req.read()
-        print res
-        return id, oauth.OAuthToken.from_string(res)
+        return oauth.OAuthToken.from_string(res)
+
+    def prepare_oauth_request(self, url, token, method='GET', **args):
+        oa_req = (oauth.OAuthRequest
+                  .from_consumer_and_token(self.consumer,
+                                           http_url=url,
+                                           http_method=method,
+                                           parameters=args,
+                                           token=token))
+        oa_req.sign_request(self.signature_method,
+                            self.consumer,
+                            token)
+        return oa_req
+
+    def get(self, url, *args, **kwargs):
+        return self.request(url, 'GET', *args, **kwargs)
+
+    def post(self, url, *args, **kwargs):
+        return self.request(url, method='POST', *args, **kwargs)
+
+    def request(self, url, method='GET', token=None, post_data=None, raw=False, **args):
+        headers = ({'Accept': 'application/json', 
+                    'X-Blip-api': '0.02'})
+        if not url.startswith('http://'):
+            url = self.protocol + self.host + url
+        if token is not None:
+            oa = self.prepare_oauth_request(url, token, method=method, **args)
+            headers.update(oa.to_header())
+        if args:
+            url = "{0}?{1}".format(url, urllib.urlencode(args))
+
+        res = self._request(url,
+                            method=method,
+                            data=post_data, 
+                            headers=headers)
+        if not raw and not str(res.status).startswith('2'):
+            raise BlipError(res.status, res.read())
+        return json.load(res) if not raw else res
--- blip/tests.py
+++ blip/tests.py
@@ -1,9 +1,25 @@
 from unittest import TestCase
 import os
+import logging
 
-KEY, SECRET, CALLBACK, TOKEN = [os.environ[l] 
-                                for l in 'KEY', 'SECRET', 'CALLBACK', 'TOKEN']
-print "Using key: %r, secret: %r, callback: %r" % (KEY, SECRET, CALLBACK)
+log = logging.getLogger("blip")
+log.setLevel(logging.DEBUG)
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+log.addHandler(ch)
+try:
+    (KEY, 
+     SECRET, 
+     CALLBACK, 
+     TOKEN) = [os.environ[l] for l in ('KEY', 
+                                          'SECRET', 
+                                          'CALLBACK', 
+                                          'USERNAME', 
+                                          'TOKEN')]
+except KeyError:
+    pass
+else:
+    print "Using key: %r, secret: %r, callback: %r" % (KEY, SECRET, CALLBACK)
 
 from blip import Blip
 from oauth import oauth
@@ -11,6 +27,7 @@ from oauth import oauth
 class TestAuth(TestCase):
     def setUp(self):
         self.blip = Blip(key=KEY, secret=SECRET, callback=CALLBACK)
+        self.token = oauth.OAuthToken.from_string(TOKEN)
 
     def test_get_request_token(self):
         token = self.blip.get_request_token()
@@ -23,10 +40,31 @@ class TestAuth(TestCase):
         print url, token
         self.assert_(isinstance(token, oauth.OAuthToken))
         self.assert_(url.startswith('http://'), url)
-        raw_input("Go to %s and accept" % url)
-        access_token = self.blip.authorize(token)
-        self.assert_(access_token)
-        print access_token
+        print url
+
+    def test_get_with_token(self):
+        res = self.blip.request('/users/szopa/updates', 
+                                token=self.token, 
+                                limit=20, 
+                                raw=True)
+        self.assertEqual(res.status, 200)
+        import json
+        json.load(res)
+
+    def test_update(self):
+        import datetime
+        text = "to jest test {0}".format(datetime.datetime.now())
+        res = self.blip.post('/updates',
+                             token=self.token,
+                             post_data={'update[body]': text},)
+                 
+        up = self.blip.get('/statuses/%s' % res['id'])
+        self.assertEqual(up['id'], res['id'])
+        self.assertEqual(up['body'], res['body'])
+
+                                
+                                
+
 
 if __name__ == '__main__':
     import unittest
